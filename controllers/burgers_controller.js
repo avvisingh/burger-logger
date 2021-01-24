dc207d3357011e729d883b81dd73a9621acb46d9
unknown <singh.avvi98@gmail.com>
2021-01-24 23:35:47 +1100

    fresh start

--- controllers/burgers_controller.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-var router = express.Router();
-
-const burgers = require('../models/burger');
-
-
-module.exports = router
--- db/seeds.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO burgers (burger_name, devoured) 
-VALUES 
-('Rebel Whopper', 1),
-("Grill'd Beyond Meat Burger", 1),
-("Nando's Plant-Based Burgers", 1);
