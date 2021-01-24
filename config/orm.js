1f679f6a7abdc288f39373a45919c8493e5f519d
unknown <singh.avvi98@gmail.com>
2021-01-24 22:04:39 +1100

    routing setup

--- config/orm.js
+++ config/orm.js
@@ -1,18 +1,21 @@
 const connection = require('./connection');
 
-const selectAll = () => {
-    query = `SELECT * FROM burgers`;
-    // connection.query()
+const selectAll = (tableName, callback) => {
+    query = `SELECT * FROM ${tableName}`;
+    connection.query(query, (error, results, fields) => {
+        if (error) console.log('Oops, something went wrong! ' + error.sqlMessage || error.stack);
+        callback(result);
+    })
 }
 
-const insertOne = () => {
+const insertOne = (tableName, callback) => {
     let userInput;
-    query = `INSERT INTO burgers (name) VALUES ("${userInput}")`
+    query = `INSERT INTO ${tableName} (name) VALUES ("${userInput}")`
 }
 
-const updateOne = () => {
+const updateOne = (tableName, callback) => {
     let toUpdate;
-    let query = `UPDATE burgers SET devoured = 0 WHERE id = ${toUpdate}`;
+    let query = `UPDATE ${tableName} SET devoured = 0 WHERE id = ${toUpdate}`;
 }
 
 module.exports = {
---
+++ controllers/burgers_controller.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+var router = express.Router();
+
+const burgers = require('../models/burger');
+
+
+module.exports = router
